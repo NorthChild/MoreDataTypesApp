3f6f149fae8f012d30aaec6ab6fe7e0b0defa6b7
michael carini <snownkc@icloud.com>
2021-07-30 12:28:00 +0100

    added to repo

--- MoreDataTypesSolution/MoreDataTypes/Program.cs
+++ MoreDataTypesSolution/MoreDataTypes/Program.cs
@@ -6,7 +6,43 @@ namespace MoreDataTypes
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            var myString = "C# list fundamentals ";
+            Console.WriteLine(StringExercise(myString));
+
         }
-    }
+
+        public static string StringExercise(string myString) 
+        {
+            // return all charachters in uppercase
+            // trim off leading or trailing soaces frin myString
+            // replace letter 'L' and 'T' with '*'
+            // find index of letter N and delete chars after it 
+            // return the result
+
+
+            var firstIter = myString.ToUpper().Trim().Replace('T', '*').Replace('L', '*');
+
+            int indexOfString = firstIter.IndexOf('N');
+
+            var finalString = firstIter.Remove(indexOfString + 1);
+
+            Console.WriteLine(finalString);
+
+
+            //Console.WriteLine(myString);
+
+            //Console.WriteLine(myString.ToUpper());
+
+            //Console.WriteLine(myString.Trim());
+
+            //Console.WriteLine(myString.Replace('T', '*'));
+            //Console.WriteLine(myString.Replace('L', '*'));
+
+            //Console.WriteLine(myString.IndexOf('N');
+
+            return myString;
+        }
+    }   
+
 }
