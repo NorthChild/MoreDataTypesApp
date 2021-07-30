64dc7986465c31f5a66f67ef8a8d19c464cae11f
michael carini <snownkc@icloud.com>
2021-07-30 12:04:37 +0100

    added dataTypesApp

---
+++ MoreDataTypesSolution/MoreDataTypes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MoreDataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
